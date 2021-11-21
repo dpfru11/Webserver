df9a29097879ef6640dbcb173bc82b4dfa4b90aa
Daniel Patrick Fruland <dfruland@data.cs.purdue.edu>
2021-11-21 02:26:58 -0500

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -144,6 +144,7 @@ void processRequest(int socket) {
    token = strtok(str, " ");
    while (token)
    {
+      printf("token: %s\n", token);
       if (token == "GET") {
          gotGet = true;
       }
