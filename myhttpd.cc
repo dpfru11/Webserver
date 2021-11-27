5f95dc2300a67db8033d6f033dd458225a7aeded
Daniel Patrick Fruland <dfruland@data.cs.purdue.edu>
2021-11-27 01:48:45 -0500

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -198,7 +198,7 @@ void expandFilePath(char * fpath, char * cwd, int socket) {
    char * finalPath = realpath(fpath, newPath);
    printf("newpath:%s\n", newPath);
    printf("cwd: %s\n", cwd);
-   if (strlen(finalpath) < (strlen(cwd) + strlen("/http-root-dir"))) {
+   if (strlen(finalPath) < (strlen(cwd) + strlen("/http-root-dir"))) {
       sendErr(405, socket, NULL);
       return;
    }
