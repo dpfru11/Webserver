050b7e546f08c876208e7e25b886334301f34672
Daniel Patrick Fruland <dfruland@data.cs.purdue.edu>
2021-11-26 20:13:10 -0500

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -149,7 +149,7 @@ void processRequest(int socket) {
       }
       if (foundDPath == true && str[i] == ' ') {
          docpath[dPathSize] = '\0';
-         
+         printf("%s\n",docpath);
          break;
       }
       if (foundDPath == true) {
