ee0aaba9226d6d9e21b845b7431f0a35b0c1ce0b
Daniel Patrick Fruland <dfruland@data.cs.purdue.edu>
2021-11-28 02:55:32 -0500

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -123,7 +123,7 @@ int main(int argc, char** argv)
          pthread_t tid;
 			pthread_attr_t attr;
 			pthread_attr_init(&attr);
-         pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+         pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 
          //pthread_create(&tid, &attr, (void * (*)(void*))processRequestThread,(void *)slaveSocket);
          pthread_create(&tid, &attr, (void *(*)(void *))processRequestThread, (void *)slaveSocket);
