593869ad894ac65cd983ee9b584fd2e6567f68ec
Daniel Patrick Fruland <dfruland@data.cs.purdue.edu>
2021-11-28 03:27:05 -0500

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -381,7 +381,7 @@ const char * contentType(char * str) {
 void processRequestThread(int * socket) {
    int sock = socket[0];
    processRequest(sock);
-   close(slaveSocket);
+   close(socket);
 }
 
 void poolSlave(int socket){
