2882af5117cf039e817280cb6f987c9d5191ad91
Daniel Patrick Fruland <dfruland@data.cs.purdue.edu>
2021-11-28 22:56:23 -0500

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -283,13 +283,13 @@ void processRequest(int socket) {
    filepath = realpath(filepath, newPath);
    expandFilePath(newPath, cwdCopy, socket);
    
-   delete cwd;
+   delete [] cwd;
    cwd = NULL;
-   delete docpath;
+   delete [] docpath;
    docpath = NULL;
-   delete newPath;
+   delete [] newPath;
    newPath = NULL;
-   delete cwdCopy;
+   delete [] cwdCopy;
    cwdCopy = NULL;
    filepath = NULL;
    
