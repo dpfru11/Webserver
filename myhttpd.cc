d733081af75d970bbc51ef54f28e4821ce42b5ec
Daniel Patrick Fruland <dfruland@data.cs.purdue.edu>
2021-12-05 00:27:59 -0500

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -364,8 +364,8 @@ void processDir(int socket, DIR * dirp, char * fpath) {
 
    //Look for the modifiers in the path
    char * possibleChoices[] = {"?C=M;O=A", "?C=M;O=D", "?C=N;O=A", "?C=N;O=D", "?C=S;O=A", "?C=S;O=D", "?C=D;O=A", "?C=D;O=D"};
-   for (int i = 0; i < possibleChoices.length; i++) {
-      if (strncmp(fpath, possibleChoices[i]) == 0) {
+   for (int i = 0; i < sizeof(possibleChoices); i++) {
+      if (strncmp(fpath, possibleChoices[i], strlen(fpath)) == 0) {
          C = possibleChoices[i][3];
          O = possibleChoices[i][7];
       }
