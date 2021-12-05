1b0ffaaa94a14544b8a0318d4e2721ef8a4f70b2
Daniel Patrick Fruland <dfruland@data.cs.purdue.edu>
2021-12-04 23:35:02 -0500

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -275,7 +275,7 @@ void processRequest(int socket) {
       filepath = strcat(filepath, docpath);
    } else if (strstr(docpath, ".") == NULL) {
       filepath = strcat(cwd, docpath);
-      printf("%s\n", filepath);
+      printf("fp: %s\n", filepath);
    } else {
       filepath = strcat(cwd, "/http-root-dir/htdocs");
       filepath = strcat(filepath, docpath);
