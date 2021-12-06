6dca4d50aab0a792792d575208d2c2e2a64ff8db
Daniel Patrick Fruland <dfruland@data.cs.purdue.edu>
2021-12-05 20:44:58 -0500

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -316,9 +316,8 @@ void expandFilePath(char * fpath, char * cwd, int socket) {
    printf("path: %s\n", fpath);
    if (strstr(fpath, ".") == NULL) {
       printf("di\n");
-      strcat(fpath, "/");
       DIR * dirp = opendir(fpath);
-      printf("oyoyoy");
+      printf("oyoyoy\n");
       if ((dir = readdir(dirp)) != NULL) {
          printf("in here?");
          processDir(socket, dirp, fpath);
